44b003c650a42f3ebd69d137f6721a405f8fe4c8
Brandon Roberts <robertsbt@gmail.com>
2024-07-27 16:20:37 -0500

    feat: add Storybook configuration with Vite builder

--- apps/my-app/.storybook/main.ts
+++ apps/my-app/.storybook/main.ts
@@ -1,16 +1,85 @@
-import type { StorybookConfig } from '@storybook/angular';
+import type { UserConfig } from 'vite';
 
-const config: StorybookConfig = {
-  stories: ['../src/app/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-  addons: ['@storybook/addon-essentials', '@storybook/addon-interactions'],
+const config = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
   framework: {
     name: '@storybook/angular',
     options: {},
   },
+  core: {
+    builder: {
+      name: '@storybook/builder-vite',
+      options: {
+        viteConfigPath: undefined,
+      },
+    },
+  },
+  async viteFinal(config: UserConfig) {
+    // Merge custom configuration into the default config
+    const { mergeConfig } = await import('vite');
+    const { default: angular } = await import('@analogjs/vite-plugin-angular');
+
+    /**
+     * Replace imports of "@storybook/angular" with "@storybook/angular/dist/client"
+     */
+    const storybookAngularImportPlugin = () => ({
+      name: '@storybook/angular',
+      config() {
+        return {
+          build: {
+            minify: false,
+            rollupOptions: {
+              plugins: [
+                {
+                  name: 'disable-compiler-treeshake',
+                  transform(_code: string, id: string) {
+                    if (id.includes('compiler')) {
+                      console.log('compiler.mjs', id);
+                      return { moduleSideEffects: 'no-treeshake' };
+                    }
+
+                    return;
+                  },
+                },
+              ],
+            },
+          },
+        };
+      },
+      transform(code: string) {
+        if (code.includes('"@storybook/angular"')) {
+          return code.replace(
+            /\"@storybook\/angular\"/g,
+            '"@storybook/angular/dist/client"'
+          );
+        }
+
+        return;
+      },
+    });
+
+    return mergeConfig(config, {
+      // Add dependencies to pre-optimization
+      optimizeDeps: {
+        include: ['react/jsx-dev-runtime', '@storybook/blocks', 'tslib'],
+      },
+      plugins: [
+        angular({
+          jit: true,
+          tsconfig: 'apps/my-app/.storybook/tsconfig.json',
+        }),
+        storybookAngularImportPlugin(),
+      ],
+    });
+  },
+  docs: {
+    autodocs: 'tag',
+  },
 };
 
 export default config;
-
-// To customize your webpack configuration you can use the webpackFinal field.
-// Check https://storybook.js.org/docs/react/builders/webpack#extending-storybooks-webpack-config
-// and https://nx.dev/recipes/storybook/custom-builder-configs
---
+++ apps/my-app/src/stories/button.component.ts
@@ -0,0 +1,87 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'storybook-button',
+  standalone: true,
+  imports: [CommonModule],
+  template: ` <button
+    type="button"
+    (click)="onClick.emit($event)"
+    [ngClass]="classes"
+    [ngStyle]="{ 'background-color': backgroundColor }"
+  >
+    {{ label }}
+  </button>`,
+  styles: `
+    .storybook-button {
+      font-family: 'Nunito Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
+      font-weight: 700;
+      border: 0;
+      border-radius: 3em;
+      cursor: pointer;
+      display: inline-block;
+      line-height: 1;
+    }
+    .storybook-button--primary {
+      color: white;
+      background-color: #1ea7fd;
+    }
+    .storybook-button--secondary {
+      color: #333;
+      background-color: transparent;
+      box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
+    }
+    .storybook-button--small {
+      font-size: 12px;
+      padding: 10px 16px;
+    }
+    .storybook-button--medium {
+      font-size: 14px;
+      padding: 11px 20px;
+    }
+    .storybook-button--large {
+      font-size: 16px;
+      padding: 12px 24px;
+    }  
+  `,
+})
+export class ButtonComponent {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  @Input()
+  primary = false;
+
+  /**
+   * What background color to use
+   */
+  @Input()
+  backgroundColor?: string;
+
+  /**
+   * How large should the button be?
+   */
+  @Input()
+  size: 'small' | 'medium' | 'large' = 'medium';
+
+  /**
+   * Button contents
+   *
+   * @required
+   */
+  @Input()
+  label = 'Button';
+
+  /**
+   * Optional click handler
+   */
+  @Output()
+  onClick = new EventEmitter<Event>();
+
+  public get classes(): string[] {
+    const mode = this.primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+
+    return ['storybook-button', `storybook-button--${this.size}`, mode];
+  }
+}
